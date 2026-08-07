6e60ebc7a50b4e6343f8d1f9d52f29f3c30f3ef1
TomasWelliam2026 <tomas.welliam2004@outlook.com>
2026-08-07 16:29:25 +0800

    start project with typescript and tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import Header from '../src/layout/header';
+import Footer from './layout/footer.';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+
+  );
+}
+
+export default App;
