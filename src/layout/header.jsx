c80ea85787e14e7b3c3d4e610376edd90ea9411a
Kevin <kevin.adrian9415@gmail.com>
2026-08-09 06:03:31 +0800

    complete header

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -2,37 +2,43 @@ import Logo from '../assets/img/6a0c63265ec9ccc9e7f030dd_Riley Logo.svg'
 
 const Header = () => {
     return (
-        <header className='flex relative'>
-            <div className='w-full h-16 absolute top-0 left-0 z-20'>
-                <div className="w-9/12 flex mx-auto justify-between px-16">
-                    <div className='flex w-2/3 relative h-16'>
-                        <div className="w-64 h-16 absolute">
-                            <img className="h-full mx-1" width='113' src={Logo} alt='logo' />
+        <header className='flex'>
+            <div className='fixed top-0 left-0 w-[1905px] z-50 bg-white h-[64px] px-[20px]'>
+                <div className="w-[1440px] flex mx-auto justify-between h-full px-[64px]">
+                    <div className="w-full flex items-center relative ">
+                        <div className="w-[137px] h-[35.44px] float-left absolute">
+                            <img className="h-full mx-[12px]" width='113' src={Logo} alt='logo' />
                         </div>
-                        <div className='w-full flex justify-end ml-32 pl-32'>
-                            <div className='text-center flex justify-center text-gray-600 items-center cursor-pointer px-4'>
-                                Team
-                                <select className='pl-1' ></select>
+                        <div className='w-full h-full flex justify-between items-center'>
+                            <div className="w-full h-full flex">
+                                <div className='flex w-full'>
+                                    <div className='w-[716.11px] flex ml-[185.234px] pl-[200px] items-center'>
+                                        <div className='relative text-center text-[14px] flex justify-center text-gray-600 items-center cursor-pointer p-[20px] pr-[40px]'>
+                                            Team
+                                            <select className='flex w-[14.39px] absolute m-[20px] ml-0 top-0 bottom-0 right-0 align-left' ></select>
+                                        </div>
+                                        <div className='relative text-center text-[14px] flex justify-center text-gray-600 items-center cursor-pointer p-[20px] pr-[40px]'>
+                                            UseCase
+                                            <select className='flex w-[14.39px] absolute m-[20px] ml-0 top-0 bottom-0 right-0 align-left' ></select>
+                                        </div>
+                                        <div className='relative text-center text-[14px] flex justify-center text-gray-600 items-center cursor-pointer p-[20px] pr-[40px]'>
+                                            Resources
+                                            <select className='flex w-[14.39px] absolute m-[20px] ml-0 top-0 bottom-0 right-0 align-left' ></select>
+                                        </div>
+                                        <div className='relative text-center text-[14px] flex justify-center text-gray-600 items-center cursor-pointer px-[10px] py-[5px] mx-[5px] mr-[23px] h-[20px]'>
+                                            CustomerStories
+                                        </div>
+                                    </div>
+                                    <div className='flex w-full justify-end items-center'>
+                                        <div className='text-center flex justify-center text-gray-600 items-center  px-[10px] py-[5px] mx-[5px] mr-[23px] h-[28px]'>
+                                            Login
+                                        </div>
+                                        <div className='text-[14px] text-center cursor-pointer flex justify-center text-gray-600 items-center px-[10px] py-[5px] mr-[23px] h-[28px]'>
+                                            Book a demo
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div className='text-center flex justify-center text-gray-600 items-center cursor-pointer px-4'>
-                                Use Case
-                                <select className='pl-1' ></select>
-                            </div>
-                            <div className='text-center flex justify-center text-gray-600 items-center cursor-pointer px-4'>
-                                Resources
-                                <select className='pl-1' ></select>
-                            </div>
-                            <div className='text-center flex justify-center text-gray-600 items-center cursor-pointer px-4'>
-                                CostomerStories
-                            </div>
-                        </div>
-                    </div>
-                    <div className='flex w-1/3 justify-end'>
-                        <div className='text-center flex justify-center text-gray-600 items-center px-6'>
-                            Login
-                        </div>
-                        <div className='text-center flex justify-center text-gray-600 items-center px-6'>
-                            Book a Demo
                         </div>
                     </div>
                 </div>
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -231,7 +231,7 @@ const HomePage = () => {
                                 </div>
                             </div>
                             <div className="w-full flex justify-center">
-                                <input className="w-1/3 border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your email to get started" />
+                                <input className="w-1/3 border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black" placeholder="Enter your email to get started" />
                             </div>
                             <div className="w-full flex justify-center mb-40">
                                 <div className="w-1/3 bg-gray-800 rounded-lg text-white text-md text-center px-3 py-2 mt-8">
